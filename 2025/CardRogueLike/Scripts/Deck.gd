b1e2e21565a514fd58e565b4fb7d6ad5e9a39419
ekurerice <alexandria.rindybell@gmail.com>
2025-12-30 11:12:14 +0900

    update

--- 2025/CardRogueLike/Scripts/Deck.gd
+++ 2025/CardRogueLike/Scripts/Deck.gd
@@ -4,21 +4,22 @@ class_name Deck
 
 const CARD_SCENE_PATH = "res://Scenes/Card.tscn"
 const CARD_DRAW_SPEED = 1
-var player_deck: Array[String] = ["Knight", "Knight", "Knight"]
-
+var player_deck: Array[String] = ["Knight", "Archer", "Demon", "Knight"]
+var card_database_reference
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	player_deck.shuffle()
 	$RichTextLabel.text = str(player_deck.size())
-
+	card_database_reference = preload("res://Scripts/CardDatabase.gd")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 func draw_card():
-	var card_drawn = player_deck[0]
-	player_deck.erase(card_drawn)
+	var card_drawn_name = player_deck[0]
+	player_deck.erase(card_drawn_name)
 	if player_deck.size() == 0:
 		$Area2D/CollisionShape2D.disabled = true
 		$Sprite2D.visible = false
@@ -29,6 +30,31 @@ func draw_card():
 	
 	var card_scene = preload(CARD_SCENE_PATH)
 	var new_card: Card = card_scene.instantiate() as Card
+	var card_image_path = str("res://Assets/" + card_drawn_name + "Card.png")
+	
+	# 画像のアサイン
+	var texture = load(card_image_path)
+	# 目標のサイズ（例: 200x300ピクセル）
+	new_card.get_node("CardImage").texture = texture
+	# 目標のサイズ（例: 200x300ピクセル）
+	var target_size = Vector2(200, 300)
+	var tex_size = texture.get_size()
+	new_card.get_node("CardImage").scale = Vector2(target_size.x / tex_size.x, target_size.y / tex_size.y)
+
+	# set_card_image(new_card.get_node("CardImage").texture, load(card_image_path))
+	new_card.get_node("Attack").text = str(card_database_reference.CARDS[card_drawn_name][0])
+	new_card.get_node("Health").text = str(card_database_reference.CARDS[card_drawn_name][1])
 	$"../CardManager".add_child(new_card)
 	new_card.name = "Card"
 	$"../PlayerHand".add_card_to_hand(new_card, CARD_DRAW_SPEED)
+	new_card.get_node("AnimationPlayer").play("card_flip")
+
+func set_card_image(sprite: Sprite2D, tex: Texture2D):
+	sprite.texture = tex
+	
+	# 目標のサイズ（例: 200x300ピクセル）
+	var target_size = Vector2(100, 150)
+	
+	# テクスチャのサイズからスケールを自動計算
+	var tex_size = tex.get_size()
+	sprite.scale = Vector2(target_size.x / tex_size.x, target_size.y / tex_size.y)
